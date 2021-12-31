eec91a74bd3e19ff5582c3a20e57a0f728c7515a
carlo <littleark@gmail.com>
2021-12-31 12:04:21 +0000

    added a test for log axis

--- test/chrts/charts/chrts.js
+++ test/chrts/charts/chrts.js
@@ -2,6 +2,7 @@ export { default as noData } from './no-data';
 export { default as linearXaxisLinearYaxis } from './linear-x-axis-y-axis';
 export { default as linearXaxisLogYaxis } from './linear-x-axis-log-y-axis';
 export { default as logXaxisLogYaxis } from './log-x-axis-log-y-axis';
+export { default as logXaxisLogYaxisWithParameter } from './log-x-axis-log-y-axis-with-parameter';
 export { default as ordinalXaxisLinearYaxis } from './ordinal-x-axis-linear-y-axis';
 export { default as linearXaxisOrdinalYaxis } from './linear-x-axis-ordinal-y-axis';
 export { default as linearFixedZeroXaxisOrdinalYaxis } from './linear-x-axis-fixed-zero-ordinal-y-axis';
---
+++ test/chrts/charts/log-x-axis-log-y-axis-with-parameter.js
@@ -0,0 +1,33 @@
+import * as chrt from 'chrt';
+import {xAxis,yAxis} from '../../../src/chrtAxis'
+
+const data = [
+  {
+    x: 5,
+    y: 10
+  },
+  {
+    x: 0,
+    y: 1
+  },
+  {
+    x: 944,
+    y: 1020
+  },
+  {
+    x: 500,
+    y: 220000
+  }
+];
+
+export default async function(container) {
+  const chart = chrt.Chrt()
+    .node(container)
+    .size(600, 200)
+    .data(data)
+    .x({domain:[0,10000], scale:'log'})
+    .y({domain:[1,100000],scale:'log'})
+    .add(xAxis(2))
+    .add(yAxis(2))
+  return chart
+}
