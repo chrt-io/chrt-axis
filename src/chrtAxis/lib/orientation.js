ff0756221c68561bf0eab5fcfa4ceaacb3aa4b69
carlo <littleark@gmail.com>
2023-05-05 21:57:07 +0200

    removed commented out code

--- src/chrtAxis/lib/orientation.js
+++ src/chrtAxis/lib/orientation.js
@@ -1,14 +1,5 @@
 import { utils } from 'chrt-object';
-// import { ORIENTATIONS, DEFAULT_ORIENTATION } from '../../constants';
-//
-// const { isNull } = utils;
 
 export default function orient(value) {
-  // let orientation = value;
-  // if(!isNull(orientation)) {
-  //   orientation = ORIENTATIONS[this.coord].indexOf(value) > -1
-  //     ? value
-  //     : DEFAULT_ORIENTATION[this.coord];
-  // }
   return this.attr('orientation', value)
 }
