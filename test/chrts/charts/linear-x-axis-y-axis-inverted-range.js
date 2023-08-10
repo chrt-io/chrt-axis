eb20b3478e6e7ae663d626dfa4b49b144dd19fb6
carlo <littleark@gmail.com>
2023-08-10 19:00:24 +0530

    upgraded packages, using @rollup/plugin-terser

--- test/chrts/charts/linear-x-axis-y-axis-inverted-range.js
+++ test/chrts/charts/linear-x-axis-y-axis-inverted-range.js
@@ -1,13 +1,13 @@
 import * as chrt from 'chrt';
-import {xAxis,yAxis} from '../../../src/chrtAxis'
+import { xAxis, yAxis } from '../../../src/chrtAxis'
 
 export default async function(container) {
   const chart = chrt.Chrt()
-    .size(300,300)
+    .size(300, 300)
     .node(container)
-    .x({range:[300,0]})
-    .y({range:[0,300]})
-    .data([2,0,3,10,4,2,1])
+    .x({ range: [300, 0] })
+    .y({ range: [0, 300] })
+    .data([2, 0, 3, 10, 4, 2, 1])
     .add(yAxis())
     .add(xAxis())
   return chart
